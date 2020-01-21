9b2ca3c694e90bbb8b41e8f8de33bcf15b697952
sguillen2 <45013630+sguillen2@users.noreply.github.com>
2020-01-21 14:37:10 -0800

    Add files via upload

---
+++ ballin.rb
@@ -0,0 +1,61 @@
+# Welcome to Sonic Pi v3.1
+use_bpm 102
+#first measure
+live_loop :loop1 do
+  sleep 1
+  play :b4
+  sleep 1
+  play :b4
+  sleep 0.5
+  play :a4
+  sleep 0.5
+  play :a4
+  sleep 0.5
+  play :g4
+  sleep 0.5
+  
+  #second measure
+  play :g4
+  sleep 1
+  play :b4
+  sleep 1
+  play :b4
+  sleep 0.5
+  play :a4
+  sleep 0.5
+  play :a4
+  sleep 0.5
+  play :g4
+  sleep 0.5
+  
+  
+  #third measure
+  play :g4
+  sleep 1
+  play :b4
+  sleep 1
+  play :b4
+  sleep 0.5
+  play :a4
+  sleep 0.5
+  play :a4
+  sleep 0.5
+  play :e4
+  sleep 0.5
+  
+  #fourth measure
+  play :g4
+  sleep 1
+  play :b4
+  sleep 0.5
+  play :b4
+  sleep 0.5
+  play :b4
+  sleep 0.5
+  play :a4
+  sleep 0.5
+  play :a4
+  sleep 0.5
+  play :a4
+  sleep 0.5
+end
